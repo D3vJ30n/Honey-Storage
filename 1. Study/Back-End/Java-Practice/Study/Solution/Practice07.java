0d4baa827a564da89d6bee61a7d00398d354ef60
D3F <jxxdxmxx@naver.com>
2024-10-10 12:05:15 +0900

    2410101205

---
+++ 1. Study/Back-End/Java-Practice/Study/Solution/Practice07.java
@@ -0,0 +1,74 @@
+public class Practice2 {
+    public static String solution(int num) {
+        // 결과 문자열을 저장할 변수 초기화
+        // StringBuilder를 사용하면 더 효율적이지만, 예제의 단순성을 위해 String 사용
+        String result = "";
+
+        // 로마 숫자 표기법을 저장하는 배열
+        // 큰 수부터 작은 수 순으로 정렬되어 있음
+        // 특별한 경우인 빼기 표현(CM, CD, XC, XL, IX, IV)도 포함
+        String[] roman = {
+            "M",    // 1000
+            "CM",   // 900
+            "D",    // 500
+            "CD",   // 400
+            "C",    // 100
+            "XC",   // 90
+            "L",    // 50
+            "XL",   // 40
+            "X",    // 10
+            "IX",   // 9
+            "V",    // 5
+            "IV",   // 4
+            "I"     // 1
+        };
+
+        // 각 로마 숫자에 대응하는 정수값 배열
+        // roman 배열의 각 요소와 1:1 매칭됨
+        int[] values = {
+            1000,   // M
+            900,    // CM
+            500,    // D
+            400,    // CD
+            100,    // C
+            90,     // XC
+            50,     // L
+            40,     // XL
+            10,     // X
+            9,      // IX
+            5,      // V
+            4,      // IV
+            1       // I
+        };
+
+        // 배열의 인덱스를 추적하는 변수
+        int i = 0;
+
+        // 입력받은 숫자가 0이 될 때까지 반복
+        while (num > 0) {
+            // 현재 숫자가 로마 숫자 값보다 크거나 같은 동안 반복
+            // 가능한 많은 큰 단위의 로마 숫자를 사용
+            while (num >= values[i]) {
+                // 해당 로마 숫자 값을 뺌
+                num -= values[i];
+                // 결과 문자열에 해당 로마 숫자를 추가
+                result += roman[i];
+            }
+            // 다음 로마 숫자로 이동
+            i++;
+        }
+
+        // 최종 변환된 로마 숫자 문자열 반환
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // 다양한 케이스에 대한 테스트 코드
+        System.out.println(solution(3));    // III
+        System.out.println(solution(4));    // IV
+        System.out.println(solution(6));    // VI
+        System.out.println(solution(13));   // XIII
+        System.out.println(solution(26));   // XXVI
+        System.out.println(solution(1994)); // MCMXCIV
+    }
+}
--- 1. Study/Back-End/Java-Practice/Study/Solution/Practice3.java
+++ 1. Study/Back-End/Java-Practice/Study/Solution/Practice3.java
@@ -1,91 +1,77 @@
+import java.util.ArrayList;
 
 public class Practice3 {
-    public static String solution(char[] str, char[] find, char[] to) {
-        int idx = 0;
-        // 최종 변경된 데이터 담을 변수
-        String replaceStr = "";
-        // 중간 중간 업데이트에 사용할 변수
-        char[] replaceBucket = str.clone();
-
-        // 문자열 내의 모든 find 에 해당하는 부분을 찾아서 to로 변경할 것
-        // do while 로 우선 한 번 찾아보고 반복하여 진행
-        do {
-            // find 부분의 index 찾을 함수 별도로 작성
-            idx = findIndex(replaceBucket, find);
-
-            
-            if (idx != -1) {
-                // 우선 find 에 해당하는 idx 전까지는 기존 데이터로 업데이트
-                for (int i = 0; i < idx; i++) {
-                    replaceStr += replaceBucket[i];
-                }
-                
-                // 그 다음 변경할 to로 업데이트
-                for (int i = 0; i < to.length; i++) {
-                    replaceStr += to[i];
-                }
-                
-                // 그리고 find 이 후의 잔여 데이터 업데이트
-                for (int i = idx + find.length; i < replaceBucket.length; i++) {
-                    replaceStr += replaceBucket[i];
-                }
-
-                // replaceBucket 을 방금 변경된 replacStr 로 업데이트
-                replaceBucket = replaceStr.toCharArray();
-                // 그 다음 변경을 위해 다시 ""로 초기화
-                replaceStr = "";
-            }
-        } while(idx != -1);
-
-        replaceStr = new String(replaceBucket);
-        return replaceStr;
-    }
-
-    public static int findIndex(char[] str, char[] find) {
-        int idx = -1;
-        boolean isMatch = false;
-
-        for (int i = 0; i < str.length; i++) {
-            // find 의 첫 글자와 같은지 확인 + 그 때의 str 에 남은 길이가 find 보다 크거나 같은지 확인 후 각 자리 비교
-            if (str[i] == find[0] && str.length - i >= find.length) {
-                isMatch = true;
-                for (int j = 1; j < find.length; j++) {
-                    // 하나라도 같지 않은 경우 false 로 변경 후 break
-                    if (str[i + j] != find[j]) {
-                        isMatch = false;
-                        break;
-                    }
-                }
-            }
-
-            // find 부분 찾았으면 idx 업데이트 후 break
-            if (isMatch) {
-                idx = i;
-                break;
-            }
+    public static String solution(String input, String cmd) {
+        // StringBuffer를 사용하는 이유
+        // 1. 문자열을 자주 수정할 때 String보다 빠르고 효율적
+        // 2. 문자를 쉽게 추가하거나 삭제할 수 있음
+        StringBuffer sb = new StringBuffer(input);
+        
+        // ArrayList를 사용하는 이유
+        // 1. 명령어들을 쉽게 저장하고 접근할 수 있음
+        // 2. 크기가 동적으로 변할 수 있어서 편리
+        ArrayList<String> cmdArr = new ArrayList<>();
+        
+        // 명령어 문자열을 공백으로 나누어 ArrayList에 저장
+        // 이렇게 하면 각 명령어를 쉽게 처리할 수 있음
+        for (String s: cmd.split(" ")) {
+            cmdArr.add(s);
         }
-
-        return idx;
+        
+        // 커서의 초기 위치를 문자열의 맨 끝으로 설정
+        // 텍스트 에디터처럼 시작할 때 커서가 맨 끝에 있도록 해야 함
+        int curSor = sb.length();
+        
+        // 명령어 배열의 현재 위치를 나타내는 변수
+        // 이 변수를 사용해 명령어를 하나씩 처리
+        int cmdIdx = 0;
+        
+        // 모든 명령어를 처리할 때까지 반복
+        while (cmdIdx != cmdArr.size()) {
+            // 현재 처리할 명령어를 가져옴
+            String cur = cmdArr.get(cmdIdx);
+            
+            if (cur.equals("L")) {
+                // L 명령어: 커서를 왼쪽으로 한 칸 옮김
+                // Math.max를 사용해 커서가 0보다 작아지지 않도록 해야 함
+                curSor = Math.max(0, curSor - 1);
+            } else if (cur.equals("D")) {
+                // D 명령어: 커서를 오른쪽으로 한 칸 옮김
+                // Math.min을 사용해 커서가 문자열 길이를 넘지 않도록 함
+                curSor = Math.min(sb.length(), curSor + 1);
+            } else if (cur.equals("B")) {
+                // B 명령어: 커서 왼쪽의 문자를 삭제
+                // 커서가 맨 앞이면 삭제할 게 없으니 다음 명령어로 넘어감
+                if (curSor == 0) {
+                    cmdIdx++;
+                    continue;
+                }
+                // 커서 왼쪽의 문자를 삭제하고 커서 위치를 조정
+                sb.delete(curSor - 1, curSor);
+                curSor = Math.max(0, curSor - 1);
+            } else if (cur.equals("P")) {
+                // P 명령어: 다음 명령어로 주어진 문자를 커서 왼쪽에 추가
+                // 다음 명령어(추가할 문자)를 가져옴
+                String s = cmdArr.get(++cmdIdx);
+                // 커서 위치에 문자를 삽입
+                sb.insert(curSor, s);
+                // 커서를 오른쪽으로 한 칸 옮김
+                curSor += 1;
+            }
+            
+            // 다음 명령어로 넘어감
+            cmdIdx++;
+        }
+        
+        // 모든 작업이 끝난 후의 문자열을 반환
+        return sb.toString();
     }
-
+    
     public static void main(String[] args) {
-        // Test code
-        String str = "Hello Java, Nice to meet you! Java is fun!";
-        String find = "Java";
-        String to = "자바";
-
-        // 기존 String replace
-        System.out.println(str.replace(find, to));
-
-        // 자체 구현 replace
-        char[] strExArr = str.toCharArray();
-        char[] findArr = find.toCharArray();
-        char[] toArr = to.toCharArray();
-        System.out.println(solution(strExArr, findArr, toArr));
-
-        strExArr = "POP".toCharArray();
-        findArr = "P".toCharArray();
-        toArr = "W".toCharArray();
-        System.out.println(solution(strExArr, findArr, toArr));
+        // 테스트 코드: 여러 가지 경우를 테스트
+        System.out.println(solution("aba", "L B"));
+        System.out.println(solution("abcd", "P x L P y"));
+        System.out.println(solution("abc", "L L L P x L B P y"));
+        System.out.println(solution("a", "B B L L D D P a P b P c"));
     }
 }
