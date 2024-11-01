387d82a44c607e20c0949d394539eb0619fd2031
D3F <jxxdxmxx@naver.com>
2024-11-01 13:10:32 +0900

    202411010110

--- study/backend/studying/src/Main.java
+++ study/backend/studying/src/Main.java
@@ -1,29 +1,11 @@
+import practicecodingtest.zbct.zbct_4_5;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 
 class Main {
-    public static String main(String s) throws IOException {
-        String currentResult = s;
-        String previousResult;
+    public static void main(String[] args) {
 
-        // previousResult와 currentResult가 다를 때까지 반복
-        do {
-            previousResult = currentResult;
-            BufferedReader br = new BufferedReader(new StringReader(currentResult));
-            StringBuilder result = new StringBuilder();
-
-            int value;
-            for (value = br.read(); value != -1; value = br.read()) {
-                char currentChar = (char) value;
-                if (result.indexOf(String.valueOf(currentChar)) == -1) {
-                    result.append(currentChar);
-                }
-            }
-            currentResult = result.toString();
-
-        } while (!previousResult.equals(currentResult));
-
-        return currentResult;
     }
 }
